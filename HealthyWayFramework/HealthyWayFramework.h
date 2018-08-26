91fe69d4872088e7b49eb67eceeef1b2049ca991
Bill Weatherwax <waxcruz@yahoo.com>
2018-08-26 07:31:06 -0700

    Initial Commit

---
+++ HealthyWayFramework/HealthyWayFramework.h
@@ -0,0 +1,19 @@
+//
+//  HealthyWayFramework.h
+//  HealthyWayFramework
+//
+//  Created by Bill Weatherwax on 8/26/18.
+//  Copyright © 2018 waxcruz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HealthyWayFramework.
+FOUNDATION_EXPORT double HealthyWayFrameworkVersionNumber;
+
+//! Project version string for HealthyWayFramework.
+FOUNDATION_EXPORT const unsigned char HealthyWayFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HealthyWayFramework/PublicHeader.h>
+
+
